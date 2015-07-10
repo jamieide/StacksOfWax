6bd32c99187190510a5e37f35a1e346f9abcad0f
Jamie Ide <jide@veic.org>
2015-07-10 13:18:52 -0400

    HMAC checkpoint

---
+++ StacksOfWax.HmacAuth/Controllers/ArtistsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Http;
+using StacksOfWax.Shared.DataAccess;
+
+namespace StacksOfWax.HmacAuth.Controllers
+{
+    public class ArtistsController : ApiController
+    {
+        private readonly StacksOfWaxDbContext _db;
+
+        public ArtistsController()
+        {
+            _db = new StacksOfWaxDbContext();
+        }
+
+        public IHttpActionResult GetArtists()
+        {
+            var artists = _db.Artists;
+            return Ok(artists);
+        }
+
+        public IHttpActionResult GetArtist(int id)
+        {
+            var artist = _db.Artists.SingleOrDefault(x => x.ArtistId == id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(artist);
+        }
+    }
+}
---
+++ StacksOfWax.HmacAuth/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(StacksOfWax.HmacAuth.Startup))]
+
+namespace StacksOfWax.HmacAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
